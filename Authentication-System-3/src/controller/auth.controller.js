90e2a7daaca6f0c1925a06b3764bf8d8638025db
jatvishal57@gmail.com <jatvishal57@gmail.com>
2026-06-05 17:24:09 +0530

    implement login and get user details functionality in authentication controller

--- Authentication-System-3/src/controller/auth.controller.js
+++ Authentication-System-3/src/controller/auth.controller.js
@@ -48,10 +48,8 @@ const registerContoller = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
-//  RESPONSE
-const userData = await userModel
-  .findById(user._id)
-  .select("-password");
+    //  RESPONSE
+    const userData = await userModel.findById(user._id).select("-password");
 
     res.status(201).json({
       sucess: true,
@@ -59,16 +57,89 @@ const userData = await userModel
       user: userData,
     });
   } catch (error) {
-      return res.status(500).json({
-    success: false,
-    message: error.message,
-  });
-
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
+};
+const LoginContoller = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({
+        sucess: false,
+        message: "All fields are required",
+      });
+    }
+    // EXISTING USER
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        sucess: false,
+        message: "User not found",
+      });
+    }
+    // PASSWORD
+    const ispasswordmatch = await bcrypt.compare(password, user.password);
+    if (!ispasswordmatch) {
+      return res.status(401).json({
+        sucess: false,
+        message: "Invalid credentials",
+      });
+    }
+    // GENRATING_TOKEN
 
- 
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+
+      process.env.JWT_KEY,
+    );
+    // SAVE TO Cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    // RESPONSE
+    res.status(200).json({
+      sucess: true,
+      message: "user logged in",
+    });
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message,
+    });
+  }
+};
+const getmeContoller = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+     const user = await userModel.findById(decoded.id).select("-password");
+
+    res.status(200).json({
+      sucess: true,
+      message: "user logged in",
+      user,
+      
+    });
+    console.log(user);
+    
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message,
+    })
+  }
 };
 
 module.exports = {
   registerContoller,
+  LoginContoller,
+  getmeContoller,
 };
--- Authentication-System-3/src/routes/auth.route.js
+++ Authentication-System-3/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 const express= require('express');
-const { registerContoller } = require('../controller/auth.controller');
+const { registerContoller, LoginContoller, getmeContoller } = require('../controller/auth.controller');
 const router= express.Router();
 
 
 router.post("/register",registerContoller)
+router.post("/login",LoginContoller)
+router.get("/get-me", getmeContoller)
 
 
 
