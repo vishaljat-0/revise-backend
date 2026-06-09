fcab93dd6b3b436c0b0b9e878c685f3a3b932977
jatvishal57@gmail.com <jatvishal57@gmail.com>
2026-06-09 12:03:12 +0530

    Refactor image upload logic to use Cloudinary and remove ImageKit integration

--- instagram-clone/src/controllers/post.controller.js
+++ instagram-clone/src/controllers/post.controller.js
@@ -1,4 +1,6 @@
-const ImageKit = require("imagekit");
+// const ImageKit = require("imagekit");
+
+const cloudinary = require("cloudinary");
 const multer = require("multer");
 const jwt = require("jsonwebtoken");
 const postModel = require("../models/post.model");
@@ -6,10 +8,16 @@ const postModel = require("../models/post.model");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-const client = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+// const client = new ImageKit({
+//   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+//   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+//   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+// });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 const postController = async (req, res) => {
@@ -30,14 +38,22 @@ const postController = async (req, res) => {
     });
   }
 
-  const file = await client.upload({
-    file: req.file.buffer.toString("base64"),
-    fileName: req.file.originalname,
-    folder: "posts",
+  // const file = await client.upload({
+  //   file: req.file.buffer.toString("base64"),
+  //   fileName: req.file.originalname,
+  //   folder: "posts",
+  // });
+  const fileStr = `data:${req.file.mimetype};base64,${req.file.buffer.toString("base64")}`;
+
+  const file = await cloudinary.v2.uploader.upload(fileStr, {
+    resource_type: "image",
+    public_id: `post_${Date.now()}`,
+    overwrite: true,
   });
+
   const post = await postModel.create({
     caption: req.body.caption,
-    imgUrl: file.url,
+    imgUrl: file.secure_url || file.url,
     user: decoded.id,
   });
   res.status(201).json({
