d0905419a60f79e201d67eeddccfefd88b6ab38e
vishaljat-0 <jatvishal57@gmail.com>
2026-09-06 18:26:42 +0530

    Delete snitch/Backend directory

--- snitch/Backend/src/routes/authRoute.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { registerController } from "../contollers/authController.js";
-import validate from "../validation/authValidator.js";
-const router = Router();
-
-router.post("/register", validate, registerController);
-export default router;
