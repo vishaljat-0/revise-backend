11dccd4beb8002e8883100a48aefa18ab0269598
jatvishal57@gmail.com <jatvishal57@gmail.com>
2026-09-06 23:11:21 +0530

    Frotend folder created

---
+++ Snitch/Backend/server.js
@@ -0,0 +1,18 @@
+import app from "./src/app.js";
+import config from "./src/config/config.js";
+import connectDB from "./src/config/db.js";
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(config.PORT, () => {
+      console.log(`Server running on port ${config.PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to start server:", error.message);
+    process.exitCode =1;
+  }
+};
+
+startServer();
